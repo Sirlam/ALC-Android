638c85950ee5f56a43ed242161bc43d7c8d6acad
Salam Olanrewaju <salam_lanre@yahoo.com>
2019-07-15 17:53:14 +0100

    Implemented webview client

--- app/src/main/java/com/example/android/alc/AboutActivity.java
+++ app/src/main/java/com/example/android/alc/AboutActivity.java
@@ -2,17 +2,35 @@ package com.example.android.alc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 public class AboutActivity extends AppCompatActivity {
+
+    private WebView webView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
-        WebView webView = (WebView) findViewById(R.id.webview);
+        this.webView = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        WebViewClientImpl webViewClient = new WebViewClientImpl(this);
+        webView.setWebViewClient(webViewClient);
         webView.loadUrl("https://google.com");
-        //webView.setJ
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && this.webView.canGoBack()) {
+            this.webView.goBack();
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
     }
 }
---
+++ app/src/main/java/com/example/android/alc/WebViewClientImpl.java
@@ -0,0 +1,25 @@
+package com.example.android.alc;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class WebViewClientImpl extends WebViewClient {
+    private Activity activity = null;
+
+    public  WebViewClientImpl(Activity activity){
+        this.activity = activity;
+    }
+
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        if(url.indexOf("https://www.google.com") > -1 ) return false;
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        activity.startActivity(intent);
+        return true;
+    }
+}
